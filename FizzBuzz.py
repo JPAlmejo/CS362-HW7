a0d836d3c1bf59f488f42030afe59f2c3bf61fb8
JPAlmejo <perezalj@oregonstate.edu>
2021-03-03 18:14:13 -0800

    Implemented a else if statement
    Tried to implemented a definition for the fizzbuzz. I have no idea how to convert it over well in python so it may be janky with the tests.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,18 @@
-def FizzBuzz(n):
-    pass
+
+def fizzbuzz(n):
+    result = []
+    for x in range(1, n+1):
+        if x % 3 == 0 and x % 5 == 0:
+            result.append("fizz buzz")
+        elif x % 3 == 0:
+            result.append('fizz')
+        elif x % 5 == 0:
+            result.append('buzz')
+        else:
+            result.append(str(x))
+    return result
+
+def main():
+    print(', '.join(fizzbuzz(5)))
+
+main()
--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -4,7 +4,7 @@ import FizzBuzz
 class TestCaseFizzBuzz(unittest.TestCase):
 #tests if fizzbuzz even runs at all
     def test1(self):
-        self.assertEqual(FizzBuzz.FizzBuzz(100))
+        self.assertEqual(FizzBuzz.fizzbuzz(5), "1, 2, fizz, 4, buzz")
 
 if __name__== '__main__':
     unittest.main(verbosity=2)
